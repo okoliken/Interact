0ef4268e1c726633764893a6ab59224498ee8cf1
jeff <somuadinaokoli219@gmail.com>
2022-02-08 14:42:05 +0100

    added goggle fonts

--- src/App.vue
+++ src/App.vue
@@ -4,8 +4,10 @@
 </template>
 
 <style>
+@import url("https://fonts.googleapis.com/css2?family=Raleway:wght@300&family=Rubik&display=swap");
 body {
-  font-family: "Trebuchet MS", "Lucida Sans Unicode", "Lucida Grande",
-    "Lucida Sans", Arial, sans-serif;
+  /* font-family: "Trebuchet MS", "Lucida Sans Unicode", "Lucida Grande",
+    "Lucida Sans", Arial, sans-serif; */
+  font-family: "Rubik", sans-serif;
 }
 </style>

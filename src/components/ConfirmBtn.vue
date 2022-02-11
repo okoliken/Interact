32bfb19f94418e9ae8afe7772a2876ed0e617506
jeff <somuadinaokoli219@gmail.com>
2022-02-11 11:51:36 +0100

    buttons for modal

---
+++ src/components/ConfirmBtn.vue
@@ -0,0 +1,5 @@
+<template>
+  <button class="px-8 py-3 rounded-lg text-white uppercase text-md">
+    <slot></slot>
+  </button>
+</template>
--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -1,15 +1,25 @@
+<script setup>
+import ConfirmBtnVue from "./ConfirmBtn.vue";
+</script>
 <template>
   <teleport to=".entryModalcontainer">
     <div
       class="bg-black bg-opacity-80 h-screen w-full fixed top-0 left-0 flex items-center justify-center px-5 sm:px-12 md:px-0"
     >
-      <div class="bg-white p-4">
+      <div class="bg-white p-5 w-4/5 md:w-96 rounded-lg">
         <div>
-          <h2 class="text-xl font-semibold">Delete comment</h2>
-          <p>
+          <h2 class="text-2xl font-semibold text-gray-800 mb-5">
+            Delete comment
+          </h2>
+          <p class="text-gray-500">
             Are you sure you want to delete this comment? This will remove the
             and can't be undone
           </p>
+
+          <div class="flex items-center py-5">
+            <ConfirmBtnVue class="bg-gray-600">no, cancel </ConfirmBtnVue>
+            <ConfirmBtnVue class="bg-red-500 ml-2">yes, delete </ConfirmBtnVue>
+          </div>
         </div>
       </div>
     </div>
